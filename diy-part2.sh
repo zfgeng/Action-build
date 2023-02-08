c00d68156a91c2e156293726127761ed19381cfd
zfgeng <79776797+zfgeng@users.noreply.github.com>
2023-02-08 21:18:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+#Disable mwan3 by default
+sed -i 's/option enabled '1'/option enabled '0'/g' package/net/mwan3/files/etc/config/mwan3
